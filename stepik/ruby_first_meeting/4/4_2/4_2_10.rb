8a4d795d706292b1b41184ec567b1e728f10cc3b
Anton Fedosov <fedosov-toxa@mail.ru>
2024-09-11 12:44:16 +0300

    4_2 done

---
+++ stepik/ruby_first_meeting/4/4_2/4_2_10.rb
@@ -0,0 +1,7 @@
+arr1.uniq!
+arr1 = arr1.sort
+puts arr1[3]
+puts arr1.length
+puts arr1.include?(7)
+puts arr1.max
+print arr1
---
+++ stepik/ruby_first_meeting/4/4_2/4_2_5.rb
@@ -0,0 +1,5 @@
+arr_new = arr1 & arr2
+arr_new = arr_new | arr3
+for el in arr_new
+    puts el
+end
